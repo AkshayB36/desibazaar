d5e7dbb6356d4173a0abf9834a24d2c8d7946486
vardalaud <vardalaud@gmail.com>
2015-04-25 14:08:03 -0400

    example dao test

--- desibazaar-rest/src/test/java/com/desibazaar/rest/ItemDaoTest.java
+++ desibazaar-rest/src/test/java/com/desibazaar/rest/ItemDaoTest.java
@@ -1,9 +1,32 @@
 package com.desibazaar.rest;
 
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.desibazaar.rest.dao.IItemDao;
+
 /**
  * @author Varda Laud
  *
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(locations = {
+		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+@WebAppConfiguration
 public class ItemDaoTest {
+	@Autowired
+	private IItemDao itemDao;
 
+	@Test
+	public void getAuctions() throws Exception {
+		itemDao.getAuctions("");
+	}
 }
